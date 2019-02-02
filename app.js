51f2824d7e52b31e2f48e2dadf25c3f682dcad14
mahfuz hussain <45997926+mahfuz-hussain@users.noreply.github.com>
2019-02-02 05:30:40 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+$(document).ready(function(){
+   $("#starterpage").show()
+   $("#game-wrap").hide()
+   $("#letsplay").click(function(){
+      $("#starterpage").hide()
+     $("#game-wrap").show();
+     $(".stay-content").show();
+     $(".ranc").html("<img src='img/com.png'>")
+     $(".player-content").hide();
+     //reset everything;
+     reset()
+  })
+
+     $(".exit").click(function(){
+     $("#game-wrap").hide();
+     $("#starterpage").show()
+  })
+
+
+
+
+$("button").click(function(){
+   $(".stay-content").hide()
+   $(".player-content").show()
+   let arr = ["rock.png","paper.png","scisso.png"];
+   let arrrm = arr[Math.floor(Math.random()*arr.length)];
+   
+   $(".ranc").html("<img class='rancimg' src ="+arrrm+">");
+   
+               
+              let hum = $(this).attr("id").length
+              let com = $(".ranc img").attr("src").length
+             //just compare the attr length where 
+             //rock.png = 8; paper.png =9; scisso.png = 10;
+              if(hum === com){
+              $(".demo").html("its a tie")
+              }
+              else if(hum === 9 && com === 8 || hum === 8 && com === 10 || hum === 10 && com === 9)
+              {
+                  if(hum === 9 && com === 8){
+                  $(".demo").html("Paper Wraps Rock")
+                  } 
+                  if(hum === 8 && com === 10){
+                  $(".demo").html("Rock Smashes seissor")
+                  }
+                  if(hum === 10 && com === 9){
+                     $(".demo").html("seissor cuts paper")
+                  }
+                  humcount++
+                  $(".humcount").html(humcount)
+            }
+            else{                  
+                  if(hum === 8 && com === 9){
+                  $(".demo").html("Paper Wraps Rock")
+                  } 
+                  if(hum === 10 && com === 8){
+                  $(".demo").html("Rock smashes seissor")
+                  }
+                  if(hum === 9 && com === 10){
+                     $(".demo").html("seissor cuts paper")
+                  }
+                  comcount++
+                  $(".comcount").html(comcount)
+            }
+               
+           
+             if(hum===8){
+                $(".rock").css("visibility","visible")
+                $(".paper , .scissors").css("visibility","hidden")
+             }else if(hum===9){
+                $(".paper").css("visibility","visible")
+                $(".rock , .scissors").css("visibility","hidden")
+             } else{
+                $(".scissors").css("visibility","visible")
+                $(".paper , .rock").css("visibility","hidden")
+             }
+        
+           
+   
+   })
+   $(".reset").click(function(){
+  reset()
+   })   
+    function reset(){
+      humcount = 0;
+      comcount= 0;
+      $(".comcount").html(comcount)
+      $(".humcount").html(humcount)
+      }
+let humcount=0;
+let comcount=0;
+
+   })
+   
+
+   
+  
